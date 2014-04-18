41da0180c4bbe9119009bf818c09de4836f39c99
Isaac Elias <isaac@milyoni.com>
2014-04-18 08:56:05 -0700

    changed some controllers more to do

--- app/controllers/family_members_controller.rb
+++ app/controllers/family_members_controller.rb
@@ -1,6 +1,24 @@
-class AccountsController < ApplicationController
+class FamilyMembersController < ApplicationController
+
   def index
-    @accounts = Account.all
+    @family_member = Account.find(params[:account_id).family_members
+  end
+
+  def new
+    @family_member = Account.find(params[:account_id]).family_members.new
+  end
+
+  def create
+    @family_member = Account.family_members.new(name: params[:name], color: params[:color])
+    if @family_member.save
+      flash[:notice] =  "Action successfully created." 
+      redirect_to root_path
+    else 
+      flash[:error] =  "There was a problem saving the action."
+      redirect_to root_path
+    end
+  
   end
   
 end
+  
--- app/controllers/member_actions_controller.rb
+++ app/controllers/member_actions_controller.rb
@@ -1,6 +1,23 @@
-class AccountsController < ApplicationController
+class MemberActionsController < ApplicationController
+
   def index
-    @accounts = Account.all
+    @family_member = FamilyMember.find(params[:family_member_id]).member_actions
+  end
+
+  def new
+    @member_action = FamilyMember.find(params[:family_member_id]).member_actions.new
+  end
+
+  def create
+    @member_action = FamilyMember.member_actions.new( family_member_id: params[:family_member_id], action_id: params[:action_id])
+    if @member_action.save
+      flash[:notice] =  "Action successfully created." 
+      redirect_to root_path
+    else 
+      flash[:error] =  "There was a problem saving the action."
+      redirect_to root_path
+    end
+  
   end
   
 end
