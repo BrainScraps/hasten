c1286d840972135296fd9f13dc71a9b7925fccd0
Isaac Elias <isaac@milyoni.com>
2014-05-05 00:05:15 -0700

    added seed file, changed some views

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,23 @@
 class AccountsController < ApplicationController
+  before_action :set_account, only: [:show, :edit, :update]
   def index
     @accounts = Account.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+private 
+
+  def set_account
+    @account = Account.includes([:family_members, :member_actions]).find(params[:id])
   end
   
 end
